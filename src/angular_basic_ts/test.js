6c7975e39442da4954fc61d8732a4d48dad11e50
Minjae <joshuara7235@spiderkim.com>
2021-09-24 10:01:48 +0900

    test typescript for build Angular

---
+++ src/angular_basic_ts/test.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+exports.AppComponent = void 0;
+var AppComponent = /** @class */ (function () {
+    function AppComponent() {
+    }
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
